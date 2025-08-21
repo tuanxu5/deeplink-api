df70aaae7f563b615a978fef7d21a55010a40398
Tuanxu5 <lehoangtuan012@gmail.com>
2025-08-21 11:15:15 +0700

    feat: first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const store = new Map();
+
+app.get("/report-open-scheme", (req, res) => {
+  const { uuid } = req.query;
+  if (!uuid) return res.status(400).send("missing uuid");
+
+  store.set(uuid, true);
+
+  setTimeout(() => store.delete(uuid), 10000);
+
+  res.send("ok");
+});
+
+app.get("/get-open-scheme", (req, res) => {
+  const { uuid } = req.query;
+  if (!uuid) return res.status(400).send("missing uuid");
+
+  if (store.has(uuid)) {
+    res.send("1");
+    store.delete(uuid);
+  } else {
+    res.send("0");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`DeepLink API running at http://localhost:${port}`);
+});
